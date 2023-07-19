428d7b530b38735268f8c5d4e6432b1fbad1849f
SemcoDynamics <bolckers19@gmail.com>
2023-07-19 19:09:42 +0200

    intialize to main branch

---
+++ tests/SwagLab.spec.js
@@ -0,0 +1,6 @@
+test("Swag labs login", async ({page}) => {
+    await page.goto("https://www.saucedemo.com/");
+    await page.locator('[data-test="username"]').type("standard_user");
+    await page.locator('[data-test="password"]').type("secret_sauce");
+    await page.locator('[data-test="login-button"]').click()
+})
--- tests/ToolsQA.spec.js
+++ tests/ToolsQA.spec.js
@@ -1,4 +1,5 @@
 const {test, expect} = require('@playwright/test');
+//npx playwright test tests/ToolsQA.spec.js
 
 
 test("Elements", async ({page}) => {
@@ -65,17 +66,70 @@ test('Check Boxes', async ({page}) => {
 
 });
 
-test.only('Radio buttons', async ({page}) => {
+test('Radio buttons', async ({page}) => {
 
+    //Opening radio button page
     await page.goto('https://demoqa.com/radio-button');
+    //Test script Varaibles
     const radioselector = page.locator('div.custom-control.custom-radio.custom-control-inline');
     const successMsg = page.locator('p.mt-3');
     const noRadio = page.locator('div.custom-control.custom-radio.custom-control-inline #noRadio')
 
+    //Click on Yes radio button
     await radioselector.nth(0).click();
+    //Print text
     console.log(await successMsg.innerText());
-    expect(await successMsg.innerText() === "You have selected NO ");
+    //Asset that the success text is display as expected
+    await expect(successMsg).toHaveText("You have selected Yes");
+    //Click on Impressive radio button
     await radioselector.nth(1).click();
+    //Print text
     console.log(await successMsg.innerText());
+    //Asset that the success text is display as expected
+    await expect(successMsg).toHaveText("You have selected Impressive");
+    //Print to see if the no button is disabled (bollean value) = true
     console.log('No radio button is disabled',await noRadio.isDisabled());
+    //Asset that the No radio button is disabled
+    await expect(noRadio).toBeDisabled();
+})
+
+test.only("Web Tables", async({page}) => {
+    //Opening Web Table page
+    await page.goto('https://demoqa.com/webtables');
+
+    //Test variables
+    const addBtn = page.locator('#addNewRecordButton');
+    const searchBox = page.locator('input#searchBox');
+    const tableBody = page.locator('.ReactTable .rt-table .rt-tbody .rt-tr-group');
+
+    //Add a new row
+    await addBtn.click();
+    //Enter Firstname
+    await page.locator('#firstName').fill('John');
+    //Enter Lastname
+    await page.locator('#lastName').fill('Sanders');
+    //Enter Email
+    await page.locator('#userEmail').fill('John@sanders.com');
+    //Enter Age
+    await page.locator('#age').fill('27');
+    //Enter Salary
+    await page.locator('#salary').fill('50000');
+    //Enter Department
+    await page.locator('#department').fill('Automation Tester');
+    //Click Submit button
+    await page.locator('#submit').click();
+    console.log(await tableBody.locator(".rt-td").nth(5).textContent()) 
+    
+    //Search the new added entry
+    //const tableBodyCount = await tableBody.count()
+    //for(let i = 0; i < tableBodyCount; i++){
+    //    const findRow = await tableBody.locator('.rt-td').nth(i).textContent()
+    //    if( findRow.trim() === "John") {
+    //        await tableBody.nth(i).locator('#edit-record-3').click()
+    //        break;
+    //    }
+    //}
+    //The $$ methond finds the elements matching and then is stored in a variable using [...listItems]
+ 
+
 })
