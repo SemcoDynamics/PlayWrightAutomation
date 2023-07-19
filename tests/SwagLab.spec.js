77347be759e871dc814ea7c1afa706ae65945f63
SemcoDynamics <bolckers19@gmail.com>
2023-07-19 19:11:33 +0200

    commit to main branch

--- tests/SwagLab.spec.js
+++ tests/SwagLab.spec.js
@@ -1,3 +1,5 @@
+const {test, expect} = require('@playwright/test');
+
 test("Swag labs login", async ({page}) => {
     await page.goto("https://www.saucedemo.com/");
     await page.locator('[data-test="username"]').type("standard_user");
