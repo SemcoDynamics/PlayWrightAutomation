ed246b32284ee2c0b46cd021c2ea4d44f8bef802
SemcoDynamics <bolckers19@gmail.com>
2023-08-20 14:16:40 +0200

    1

--- pageobjects/LoginPage.js
+++ pageobjects/LoginPage.js
@@ -9,7 +9,7 @@ class LoginPage {
     }
     async goTo()
     {
-        this.page.goTo("https://rahulshettyacademy.com/client/");
+        await this.page.goTo("https://rahulshettyacademy.com/client/");
     }
     //creating a method validLogin()
     async validLogin(email,loginPass)
